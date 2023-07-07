6d201028c7bb6a7845544ddb80ece694a16df317
Evan Stern <evanstern@machineservant.com>
2023-07-07 15:15:28 -0400

    Refactor and update code

--- app/routes/logout.tsx
+++ app/routes/logout.tsx
@@ -1,8 +1,8 @@
-import type { ActionArgs } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
+import type { ActionArgs } from '@remix-run/node';
+import { redirect } from '@remix-run/node';
 
-import { logout } from "~/session.server";
+import { logout } from '~/modules/auth';
 
 export const action = async ({ request }: ActionArgs) => logout(request);
 
-export const loader = async () => redirect("/");
+export const loader = async () => redirect('/');
