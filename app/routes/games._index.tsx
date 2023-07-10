059411e35bdfaf919d8835d098d4d272633cd721
Evan Stern <evanstern@machineservant.com>
2023-07-09 21:09:15 -0400

    Extract GameCard into its own component

--- app/routes/games._index.tsx
+++ app/routes/games._index.tsx
@@ -6,6 +6,7 @@ import { twMerge } from 'tailwind-merge';
 import { SearchIcon } from '~/components/icons';
 import { getUserId } from '~/modules/auth';
 import type { Game } from '~/modules/games';
+import { GameCard } from '~/modules/games';
 import { searchForGames } from '~/modules/games/service.server';
 import type { RawgListResponse } from '~/types';
 
@@ -79,31 +80,7 @@ export default function GamesPage() {
         )}
         {games &&
           games?.results?.length > 0 &&
-          games.results.map((game) => (
-            <div
-              key={game.id}
-              className="mb-8 flex flex-col gap-4 rounded-lg border border-slate-400 lg:flex-row"
-            >
-              <div className="flex-none lg:w-96">
-                {game.background_image ? (
-                  <img
-                    className="h-full w-full"
-                    src={game.background_image}
-                    alt={game.name}
-                  />
-                ) : (
-                  <div className="flex h-60 w-full items-center justify-center border-b border-slate-400 lg:border-b-0 lg:border-r">
-                    <h3 className="text-center text-xl font-bold text-slate-600">
-                      No image available
-                    </h3>
-                  </div>
-                )}
-              </div>
-              <div className="flex-1 px-4 pb-4 lg:px-0 lg:py-4">
-                <h3 className="text-xl font-bold">{game.name}</h3>
-              </div>
-            </div>
-          ))}
+          games.results.map((game) => <GameCard key={game.id} game={game} />)}
       </div>
       <div className="flex w-full items-center justify-between self-end">
         <Link
