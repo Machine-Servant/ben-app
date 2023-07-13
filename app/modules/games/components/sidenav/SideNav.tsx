355928920cc0e30e7084331ebf1b93c209480fe9
Evan Stern <evanstern@machineservant.com>
2023-07-13 15:44:29 -0400

    Change colors

--- app/modules/games/components/sidenav/SideNav.tsx
+++ app/modules/games/components/sidenav/SideNav.tsx
@@ -13,18 +13,18 @@ export const SideNav: React.FC<SideNavProps> = ({ user, onItemClick }) => {
       <div className="mb-auto w-full">
         <nav className="w-full">
           <ul className="flex flex-col">
-            <li className="w-full border-b hover:bg-gray-100">
+            <li className="w-full border-b border-red-500 bg-red-800 hover:bg-red-600">
               <Link
-                className="block h-full w-full px-8 py-4 text-xl"
+                className="block h-full w-full px-8 py-4 text-xl text-white"
                 to="/games"
                 onClick={onItemClick}
               >
                 Games
               </Link>
             </li>
-            <li className="w-full border-b hover:bg-gray-100">
+            <li className="w-full border-b border-red-500 bg-red-800 hover:bg-red-600">
               <Link
-                className="block h-full w-full px-8 py-4 text-xl"
+                className="block h-full w-full px-8 py-4 text-xl text-white"
                 to="favorites"
                 onClick={onItemClick}
               >
@@ -36,21 +36,21 @@ export const SideNav: React.FC<SideNavProps> = ({ user, onItemClick }) => {
       </div>
       <nav className="mt-auto w-full">
         <ul className="flex flex-col">
-          <li className="w-full border-b px-4 py-2 hover:bg-gray-100">
+          <li className="w-full border-b border-yellow-500 px-4 py-2 hover:bg-green-700">
             <div className="flex items-center gap-8">
-              <Avatar.Root className="flex h-12 w-12 items-center justify-center rounded-full border border-slate-500">
+              <Avatar.Root className="flex h-12 w-12 items-center justify-center rounded-full border border-yellow-500">
                 <Avatar.Image />
-                <Avatar.Fallback>
+                <Avatar.Fallback className="text-yellow-500">
                   {user.email.slice(0, 1).toUpperCase()}
                 </Avatar.Fallback>
               </Avatar.Root>
-              {user.email}
+              <span className="text-yellow-500">{user.email}</span>
             </div>
           </li>
           <li className="w-full hover:bg-gray-100">
             <Form method="post" action="/logout">
               <button
-                className="w-full bg-slate-800 px-4 py-2 text-white"
+                className="w-full bg-slate-800 px-4 py-2 text-white hover:bg-slate-700"
                 type="submit"
               >
                 Logout
